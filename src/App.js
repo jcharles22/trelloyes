60e837b5614b8818d632ce8858df297b3721be45
Jared Charles <jaredcharles22@gmail.com>
2019-05-02 20:00:21 -0400

    changed STORE o store instead of prop

--- src/App.js
+++ src/App.js
@@ -4,24 +4,104 @@ import List from './Components/List.js';
 import './Components/App.css';
 
 
-function App() {  
-  return(
-    <main className='App'>
-    <header className='App-header'>
-      <h1>Trelloyes!</h1>
-    </header>
-    <div className='App-list'>
-   
-      {STORE.lists.map(list => (
-        <List
-          key={list.id}
-          header={list.header}
-          cards={list.cardIds.map(id => STORE.allCards[id])}
-        />
-      ))}
-    </div>
-  </main>
-  )
+const newRandomCard = () => {
+  const id = Math.random().toString(36).substring(2, 4)
+    + Math.random().toString(36).substring(2, 4);
+  return {
+    id,
+    title: `Random Card ${id}`,
+    content: 'lorem ipsum',
+  }
+} 
+
+function omit(obj, keyToOmit) {
+  return Object.entries(obj).reduce(
+    (newObj, [key, value]) =>
+        key === keyToOmit ? newObj : {...newObj, [key]: value},
+    {}
+  );
+}
+
+class App extends React.Component {
+  constructor() {
+    
+    super()
+    
+    this.state = {
+      store : STORE
+    }
+  }  
+
+  handleDelete = (cardId) => {
+    const { lists, allCards } = this.state.store;
+
+    const newLists = lists.map(list => ({
+      ...list,
+      cardIds: list.cardIds.filter(id => id !== cardId)
+    }));
+
+    const newCards = omit(allCards, cardId);
+
+    this.setState({
+      store: {
+        lists: newLists,
+        allCards: newCards
+      }
+    })
+  };
+  
+  handleRandom = (listId) => {
+      const newCard = newRandomCard()
+  
+      const newLists = this.state.store.lists.map(list => {
+        if (list.id === listId) {
+    return {
+            ...list,
+            cardIds: [...list.cardIds, newCard.id]
+          };
+        }
+        return list;
+      })
+  
+      this.setState({
+        store: {
+          lists: newLists,
+          allCards: {
+            ...this.state.store.allCards,
+            [newCard.id]: newCard
+          }
+        }
+      })
+  }
+
+
+  render() {
+
+    const {store} = this.state;
+    return(
+      
+      <main className='App'>
+      <header className='App-header'>
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className='App-list'>
+    
+        {this.state.store.lists.map((list, index) => (
+          <List
+            key={list.id}
+            id={list.id}
+            header={list.header}
+            cards={list.cardIds.map(id => store.allCards[id])}
+            handleDelete={this.handleDelete}
+            handleRandom={this.handleRandom}
+          />
+          )
+        )}
+      </div>
+    </main>
+    
+    )
+  }
 }
 
 export default App;
--- src/Components/List.js
+++ src/Components/List.js
@@ -2,22 +2,24 @@ import React from 'react';
 import Card from './Card';
 import './List.css';
 
-function List(prop) {
+function List(props) {
     return (
         <section className="List">
-            <header class="List-header">
-                <h2>{prop.header}</h2>
+            <header className="List-header">
+                <h2>{props.header}</h2>
             </header>
             <div className="List-cards"> 
-                {prop.cards.map((card) => (
+                {props.cards.map((card, index) => (
                     <Card
-                    key={card.id}
+                    key={index}
                     title={card.title}
                     content={card.content}
+                    handleDelete={props.handleDelete}
                     />
+                    
                 )
             )}
-            <button>
+            <button onClick={() => props.handleRandom(props.id)}>
                 + Add Random Card
             </button>
             </div>
